aeb5cdb19dc2afdb5e43026591488d7e8e36d650
rastirland <rastirland@hotmail.com>
2022-04-24 15:59:10 +0100

    website

--- atom/templates/logoutform.html.php
+++
@@ -1,1 +0,0 @@
-<h1>you have logged out!</h1>
--- atom/tests/RegsiterUserTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-class RegisterUserTest extends \PHPUnit\Framework\TestCase {
-public function testInvalidEmail() {
-    $variable = false;
-
-    $this->assertTrue($variable);
-}
-}
